0b14e109c478bb2d701f216b78c3a9e028cf4723
the_haury <pascal@thehaury.de>
2018-01-01 03:18:41 +0100

    Update

---
+++ SpotSkip/readme.md
@@ -0,0 +1,56 @@
+Who doesn't know it. Everyone has some artists or songs who you hate. 
+Spotify hasn't implemented a feature where you can remove an artist or 
+a song from your life... But here comes SpotSkip!
+
+![alt tag](https://raw.githubusercontent.com/theHaury/SpotifySongSkipper/master/readme_pictures/SpotSkip.PNG "SpotSkip Maing Window")
+
+SpotSkip lets you choose what you want to hear.
+It creates a block-list in your MyDocuments/SpotSkip Folder. If a 
+song/artist/combo is playing which is listed in the block-list, 
+SpotSkip will simply skip the Song!
+
+Features:
+
+- Block Songs
+
+	You Don't like a Song and all of it's remixes/Covers.
+	It blocks all songs that contain the song-title.
+	
+- Block Artist
+
+    You don't like a certain Artist
+	It blocks all songs from the current artist.
+	
+- Block Combo
+
+    You like the song and the artist, but not a certain 
+	remix/cover.
+	It Blocks the current song from the current artist.
+	
+- BlockListManager(BLM)
+
+    You accidentally blocked something? - No Problem!
+	With the BlockListManager you can view your BlockList
+	sorted by song, artist and combo. Simply select the 
+	entry you want to unblock and press unblock. The Song
+	will now no longer be skipped.
+	
+	![alt tag](https://raw.githubusercontent.com/theHaury/SpotifySongSkipper/master/readme_pictures/BlockListManager.PNG "SpotSkip BlockListManager")
+	
+What you should know:
+
+    SpotSkip works at the moment only with Spotify Desktop or the
+    Windows 10 App. You should also close all other Multimedia software, 
+    because if SpotSkip skips a song, it sends a Next Song Multimedia 
+    key command to the OS. If any other Multimedia software is running in
+    the Background, it may get triggered and starts playing something.
+    In a (at the moment) very far future it is planned 
+    to create an Android App, and Implement the Spotify API. 
+	
+How it works:
+
+    SpotSkip reads the title of the Spotify window. The Title 
+    contains the Song and the Artist. Now SpotSkip processes
+    the string and searches for it in the BlockList. If it 
+    finds the current song/artist/combo in the BlockList
+    SpotSkip simulates a "Next Song" Multimedia button. 
---
+++ SpotSkipUpdate/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SpotSkipUpdate
+{
+    class Program
+    {
+        private static string SettingsFilePath = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\SpotSkip\Settings.xml";
+
+        static void Main(string[] args)
+        {
+            string UpdateFolderPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\Update";
+            string OldVersionPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\Old";
+            string AppPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            AppPath = AppPath.Replace(AppPath.Split('\\').Last(), "");
+
+            string OnlineVersion = string.Empty;
+            string InstalledVersion = string.Empty;
+            string Size = string.Empty;
+            string DownloadURL = string.Empty;
+            string UpdateFileName = string.Empty;
+
+            Console.WriteLine("Checking for Updates:");
+            InstalledVersion = getVersionInstalled();
+
+            foreach (string line in HttpGet("https://api.github.com/repos/theHaury/SpotifySongSkipper/releases/latest").Split(new String[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (line.StartsWith("\"tag_name\""))
+                {
+                    OnlineVersion = line.Split(':').Last().Replace("\"", "").Replace(",", "");
+
+                    Console.WriteLine("Online version: " + OnlineVersion);
+                    Console.WriteLine("Installed version: " + InstalledVersion);
+                }
+                else if (line.StartsWith("\"size\""))
+                {
+                    Size = line.Split(':').Last().Replace("\"", "").Replace(",", "");
+                    Console.WriteLine("Update size: " + CalculateFileSize(int.Parse(Size)));
+                }
+                else if (line.StartsWith("\"browser_download_url\""))
+                {
+                    DownloadURL = "https:" + line.Split(':').Last().Replace("\"", "");
+                    DownloadURL = DownloadURL.Remove(DownloadURL.Count() - 3, 3);
+                    //Console.WriteLine("URL: " + DownloadURL);
+                }
+            }
+
+            if ((double.Parse(InstalledVersion) >= double.Parse(OnlineVersion)) && InstalledVersion != "0.0")
+            {
+                Console.WriteLine("No update available...\r\nPress any key to exit...");
+                Console.ReadKey();
+            }
+            else
+            {
+                UpdateFileName = DownloadURL.Split('/').Last();
+
+
+                Console.WriteLine("\r\nDownloading new Version...");
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile(DownloadURL, UpdateFileName);
+                }
+
+                if (Directory.Exists(OldVersionPath))
+                {
+                    Directory.Delete(OldVersionPath, true);
+                }
+                Directory.CreateDirectory(UpdateFolderPath);
+                Directory.CreateDirectory(OldVersionPath);
+
+                foreach (string file in Directory.GetFiles(AppPath, "*.*", SearchOption.TopDirectoryOnly))
+                {
+                    if (!file.Contains("SpotSkipUpdate.exe"))
+                    {
+                        if (!file.Contains(UpdateFileName))
+                        {
+                            if (file.EndsWith(".exe") || file.EndsWith(".dll"))
+                            {
+                                string FileName = Path.GetFileName(file);
+                                File.Move(file, OldVersionPath + "\\" + FileName);
+                            }
+                        }
+                    }
+
+                }
+                ZipFile.ExtractToDirectory(AppPath + "SpotSkipUpdater\\" + DownloadURL.Split('/').Last(), UpdateFolderPath);
+                foreach (string file in Directory.GetFiles(UpdateFolderPath, "*.*", SearchOption.TopDirectoryOnly))
+                {
+                    string FileName = Path.GetFileName(file);
+                    File.Move(file, AppPath + "\\" + FileName);
+                }
+                File.Delete(AppPath + "SpotSkipUpdater\\" + DownloadURL.Split('/').Last());
+                Directory.Delete(UpdateFolderPath, true);
+                Console.WriteLine("Done!\r\nPress any key to exit...");
+                Console.ReadKey();
+                Console.WriteLine("Starting SpotSkip now...");
+                System.Diagnostics.Process.Start(AppPath + "\\SpotSkip.exe");
+
+            }
+        }
+
+        public static string HttpGet(string URI)
+        {
+            WebClient client = new WebClient();
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            Stream data = client.OpenRead(URI);
+            StreamReader reader = new StreamReader(data);
+            string s = reader.ReadToEnd();
+            s = s.Replace(",", ",\r\n");
+            data.Close();
+            reader.Close();
+            return s;
+        }
+
+        private static string CalculateFileSize(int inFile)
+        {
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
+            if (inFile == 0)
+                return "0" + suf[0];
+            long bytes = Math.Abs(inFile);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return (Math.Sign(inFile) * num).ToString() + suf[place];
+        }
+
+        private static string getVersionInstalled()
+        {
+            try
+            {
+                XElement root = XElement.Parse(File.ReadAllText(SettingsFilePath));
+
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(SettingsFilePath);
+                XmlNode Version = xmlDoc.SelectSingleNode("Data/Settings/Version");
+                return Version.Attributes["ProgramVersion"].Value;
+            }
+            catch (Exception ex)
+            {
+                return "0.0";
+            }
+        }
+    }
+}
